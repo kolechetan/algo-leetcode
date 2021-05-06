9fd0afeee3694e83d46b1892bf08d581d9610a63
Chetan Kole <kolechetan@gmail.com>
2021-05-06 17:38:35 +0530

    Create longest-palindromic-substring.go

---
+++ longest-palindromic-substring.go
@@ -0,0 +1,54 @@
+func longestPalindrome(s string) string {
+    slen := len(s)
+    
+    if slen == 1 {
+        return s
+    }
+    var T = make([][]int, slen+1)
+    for i := range T {
+        T[i] = make([]int, slen+1)
+    }
+    
+        ops := ""
+    max := 0
+    for i :=  0 ; i < slen ; i++ {
+        T[i+1][i+1] = 1 
+        
+        
+    }
+    max = 1
+    ops = s[0:1]
+    
+
+    for i :=0; i< slen -1 ; i++ {
+        if s[i]==s[i+1] {
+            T[i+1][i+2]=1;
+            max = 2
+            ops =  s[i:i+2]
+        }
+           
+    }
+    
+    k := 3
+    for l :=  0 ; l < slen ; l++ { 
+        i := 1
+        for j :=  k ; j <= slen; j++ {
+            if s[i-1] == s[j-1] {      
+                
+                T[i][j] = T[i+1][j-1] 
+                if j-i +1   > max {
+                    max  = j-i
+                    if T[i][j] == 1  {
+                        ops =  s[i-1:j]
+                    }
+                }
+            } else  {
+                T[i][j] = 0
+            }
+            i++
+        }
+        k++
+    }
+    
+    return ops
+}
